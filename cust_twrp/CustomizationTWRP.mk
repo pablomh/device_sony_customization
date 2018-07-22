acaaa8dfb03510ad2887eadecd4ecf825ddd4fc6
Angelo G. Del Regno <kholk11@gmail.com>
2018-07-22 17:49:47 +0200

    TWRP: Add encryption support and strace
    Full encryption support for TWRP on Sony devices.

--- cust_twrp/CustomizationTWRP.mk
+++ cust_twrp/CustomizationTWRP.mk
@@ -36,8 +36,9 @@ TW_FLASH_FROM_STORAGE := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 
-# TWRP: Encryption
-TW_INCLUDE_CRYPTO := true
+# TWRP: Encryption with FBE
+TW_INCLUDE_CRYPTO     := true
+TW_INCLUDE_CRYPTO_FBE := true
 
 # TWRP: These properties may change on a per-device basis
 TW_THEME := portrait_hdpi
@@ -64,5 +65,83 @@ ifneq ($(filter yoshino,$(PRODUCT_PLATFORM)),)
 TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/trwp_yoshino.fstab
 endif
 
-# SELinux
-BOARD_SEPOLICY_DIRS += device/sony/customization/sepolicy
+# Install kernel modules to root directory
+NEED_KERNEL_MODULE_ROOT := true
+
+# Dependencies of libsecureui.so
+TARGET_RECOVERY_DEVICE_MODULES += \
+      libEGL \
+      libGLESv2 \
+      android.hardware.graphics.allocator@2.0 \
+      android.hardware.graphics.common@1.0 \
+      android.hardware.graphics.mapper@2.0 \
+      android.hardware.configstore@1.0 \
+      android.hardware.configstore-utils \
+      android.hardware.graphics.bufferqueue@1.0 \
+      android.hardware.media@1.0 \
+      android.hidl.token@1.0 \
+      android.hidl.token@1.0-utils \
+      libbinder \
+      libgui \
+      libnativebridge \
+      libnativehelper \
+      libnativeloader \
+      libnativewindow \
+      libprotobuf-cpp-lite \
+      libsync \
+      libui
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+      $(OUT)/system/lib64/libEGL.so \
+      $(OUT)/system/lib64/libGLESv2.so \
+      $(OUT)/system/lib64/android.hardware.graphics.allocator@2.0.so \
+      $(OUT)/system/lib64/android.hardware.graphics.common@1.0.so \
+      $(OUT)/system/lib64/android.hardware.graphics.mapper@2.0.so \
+      $(OUT)/system/lib64/android.hardware.configstore@1.0.so \
+      $(OUT)/system/lib64/android.hardware.configstore-utils.so \
+      $(OUT)/system/lib64/android.hardware.graphics.bufferqueue@1.0.so \
+      $(OUT)/system/lib64/android.hardware.media@1.0.so \
+      $(OUT)/system/lib64/android.hidl.token@1.0.so \
+      $(OUT)/system/lib64/android.hidl.token@1.0-utils.so \
+      $(OUT)/system/lib64/libbinder.so \
+      $(OUT)/system/lib64/libgui.so \
+      $(OUT)/system/lib64/libnativebridge.so \
+      $(OUT)/system/lib64/libnativehelper.so \
+      $(OUT)/system/lib64/libnativeloader.so \
+      $(OUT)/system/lib64/libnativewindow.so \
+      $(OUT)/system/lib64/libprotobuf-cpp-lite.so \
+      $(OUT)/system/lib64/libsync.so \
+      $(OUT)/system/lib64/libui.so
+
+# Required for keymaster
+TARGET_RECOVERY_DEVICE_MODULES += \
+      hwservicemanager \
+      libhidl-gen-utils \
+      libtinyxml2 \
+      libvintf
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+      $(OUT)/system/bin/hwservicemanager \
+      $(OUT)/system/lib64/libhidl-gen-utils.so \
+      $(OUT)/system/lib64/libtinyxml2.so \
+      $(OUT)/system/lib64/libvintf.so
+
+# Required by android.hardware.keymaster-3.0-service
+TARGET_RECOVERY_DEVICE_MODULES      += libhardware_legacy
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/lib64/libhardware_legacy.so
+
+# Required by android.hardware.gatekeeper-1.0-service
+TARGET_RECOVERY_DEVICE_MODULES      += android.hardware.gatekeeper@1.0
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/lib64/android.hardware.gatekeeper@1.0.so
+
+# Required by android.hardware.boot-1.0-service
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
+
+# Add strace
+TARGET_RECOVERY_DEVICE_MODULES      += strace
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/xbin/strace
+
+PRODUCT_COPY_FILES += \
+    device/sony/customization/cust_twrp/manifest.xml:recovery/root/vendor/manifest.xml \
+    device/sony/customization/cust_twrp/init.twrp.rc:root/init.twrp.rc
