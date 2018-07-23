64383170211f2fe27eee00908ebbb61a53f50d64
Angelo G. Del Regno <kholk11@gmail.com>
2018-07-23 12:32:15 +0200

    TWRP: Move fstab selection and extra modules to dedicated makefiles
    Stop polluting the CustomizationTWRP.mk file with extras and
    device specific selections: this file is meant to be generic
    with common properties shared between the SoMC platforms.

--- cust_twrp/CustomizationTWRP.mk
+++ cust_twrp/CustomizationTWRP.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include device/sony/customization/cust_twrp/twrp_fstab.mk
+include device/sony/customization/cust_twrp/twrp_extra_modules.mk
+
 WITH_TWRP := true
 
 # TWRP Recovery common configuration
@@ -48,29 +51,6 @@ TW_NO_SCREEN_BLANK := true
 # init
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 
-ifneq ($(filter loire,$(SOMC_PLATFORM)),)
-TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/twrp_loire.fstab
-endif
-
-ifneq ($(filter tone,$(SOMC_PLATFORM)),)
-TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/twrp_tone.fstab
-endif
-
-ifneq ($(filter nile,$(SOMC_PLATFORM)),)
-TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/twrp_nile.fstab
-endif
-
-ifneq ($(filter yoshino,$(SOMC_PLATFORM)),)
-TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/twrp_yoshino.fstab
-endif
-
-ifneq ($(filter tama,$(SOMC_PLATFORM)),)
-TARGET_RECOVERY_FSTAB := device/sony/customization/cust_twrp/twrp_tama.fstab
-endif
-
-# Debug: Print TWRP fstab path for inclusion bug verification
-#$(warning CustomizationTWRP: Using fstab $(TARGET_RECOVERY_FSTAB) for TWRP Recovery)
-
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.$(TARGET_BOARD_PLATFORM) \
@@ -78,79 +58,5 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libz
 endif
 
-# Dependencies of libsecureui.so
-TARGET_RECOVERY_DEVICE_MODULES += \
-      libEGL \
-      libGLESv2 \
-      android.hardware.graphics.allocator@2.0 \
-      android.hardware.graphics.common@1.0 \
-      android.hardware.graphics.mapper@2.0 \
-      android.hardware.configstore@1.0 \
-      android.hardware.configstore-utils \
-      android.hardware.graphics.bufferqueue@1.0 \
-      android.hardware.media@1.0 \
-      android.hidl.token@1.0 \
-      android.hidl.token@1.0-utils \
-      libbinder \
-      libgui \
-      libnativebridge \
-      libnativehelper \
-      libnativeloader \
-      libnativewindow \
-      libprotobuf-cpp-lite \
-      libsync \
-      libui
-
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
-      $(OUT)/system/lib64/libEGL.so \
-      $(OUT)/system/lib64/libGLESv2.so \
-      $(OUT)/system/lib64/android.hardware.graphics.allocator@2.0.so \
-      $(OUT)/system/lib64/android.hardware.graphics.common@1.0.so \
-      $(OUT)/system/lib64/android.hardware.graphics.mapper@2.0.so \
-      $(OUT)/system/lib64/android.hardware.configstore@1.0.so \
-      $(OUT)/system/lib64/android.hardware.configstore-utils.so \
-      $(OUT)/system/lib64/android.hardware.graphics.bufferqueue@1.0.so \
-      $(OUT)/system/lib64/android.hardware.media@1.0.so \
-      $(OUT)/system/lib64/android.hidl.token@1.0.so \
-      $(OUT)/system/lib64/android.hidl.token@1.0-utils.so \
-      $(OUT)/system/lib64/libbinder.so \
-      $(OUT)/system/lib64/libgui.so \
-      $(OUT)/system/lib64/libnativebridge.so \
-      $(OUT)/system/lib64/libnativehelper.so \
-      $(OUT)/system/lib64/libnativeloader.so \
-      $(OUT)/system/lib64/libnativewindow.so \
-      $(OUT)/system/lib64/libprotobuf-cpp-lite.so \
-      $(OUT)/system/lib64/libsync.so \
-      $(OUT)/system/lib64/libui.so
-
-# Required for keymaster
-TARGET_RECOVERY_DEVICE_MODULES += \
-      hwservicemanager \
-      libhidl-gen-utils \
-      libtinyxml2 \
-      libvintf
-
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
-      $(OUT)/system/bin/hwservicemanager \
-      $(OUT)/system/lib64/libhidl-gen-utils.so \
-      $(OUT)/system/lib64/libtinyxml2.so \
-      $(OUT)/system/lib64/libvintf.so
-
-# Required by android.hardware.keymaster-3.0-service
-TARGET_RECOVERY_DEVICE_MODULES      += libhardware_legacy
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/lib64/libhardware_legacy.so
-
-# Required by android.hardware.gatekeeper-1.0-service
-TARGET_RECOVERY_DEVICE_MODULES      += android.hardware.gatekeeper@1.0
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/lib64/android.hardware.gatekeeper@1.0.so
-
-# Required by android.hardware.boot-1.0-service
-TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
-TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/lib64/android.hardware.boot@1.0.so
-
-# Add strace
-TARGET_RECOVERY_DEVICE_MODULES      += strace
-TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/xbin/strace
-
 PRODUCT_PACKAGES += \
-      init.recovery.usb
+    init.recovery.usb
